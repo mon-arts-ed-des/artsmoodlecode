13704067134c78133c953b6aa40de8b2df8fc989
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2020-01-06 16:13:03 +1100

    Update sectionLinkCorrection.js

--- sectionLinkCorrection.js
+++ sectionLinkCorrection.js
@@ -1,5 +1,4 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
-    $(document).ready(function(){
+$(document).ready(function(){
     $('a').each(function(){
         this.href = this.href.replace('#section-', '&section=');
     });
