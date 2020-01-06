a7024c47f86ec17ee654a454a02a627f2e62689d
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2020-01-06 15:14:23 +1100

    Update lessonWeekLink.js

--- lessonWeekLink.js
+++ lessonWeekLink.js
@@ -1,4 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" type="text/javascript"></script>
 $(document).ready(function(){
     $('a').each(function(){
         this.href = this.href.replace('#section-', '&section=');
