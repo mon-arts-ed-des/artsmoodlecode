6d114569eae401c181ef19fa07191d7d474aa5ca
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2020-01-26 12:35:56 +1100

    Update sectionLinkCorrection.js

--- sectionLinkCorrection.js
+++ sectionLinkCorrection.js
@@ -1,5 +1,6 @@
 window.onload=function(){
     $('nav a, .bookexit').each(function(){
         this.href=this.href.replace('#section-','&section=');
+    $('.editor_atto_toolbar div').removeAttr('hidden');
     });
 };
