08d25f89f19da00556d4d988fb6aa62f4380a0f9
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2020-01-31 09:27:24 +1100

    Update lessonSectionLink.js

--- lessonSectionLink.js
+++ lessonSectionLink.js
@@ -4,4 +4,6 @@ window.onload=function(){
     });
   $('.generalbox a:last-child').attr('href', $('ol.breadcrumb li:nth-last-child(2) > a').attr('href'));
   $('.generalbox a.lessonbutton:nth-child(3)').prepend('<i class="fa fa-hand-o-left" aria-hidden="true"></i> ');
+  $('.generalbox a.lessonbutton:nth-child(4)').prepend('<i class="fa fa-hand-o-right" aria-hidden="true"></i> ');
+  $('.generalbox a.lessonbutton:nth-child(5)').prepend('<i class="fa fa-calendar" aria-hidden="true"></i> ');
 };
