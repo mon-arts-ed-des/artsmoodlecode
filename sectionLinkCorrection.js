d02e77163c79e951d6fc4113b02ec43039241221
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2020-05-05 08:58:17 +1000

    Update sectionLinkCorrection.js

--- sectionLinkCorrection.js
+++ sectionLinkCorrection.js
@@ -3,6 +3,5 @@ window.onload=function(){
         this.href=this.href.replace('#section-','&section=');
     $('.editor_atto_toolbar div').removeAttr('hidden');
     $('.editor_atto_toolbar div').css('display', 'inline-block');
-    $('.howToLabel').closest('li').empty();
     });
 };
