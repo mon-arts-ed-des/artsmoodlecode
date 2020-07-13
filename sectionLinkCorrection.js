22a3ff567b8b36b6c3427ce220f36ba945133f3a
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2020-07-13 15:15:44 +1000

    Update sectionLinkCorrection.js

--- sectionLinkCorrection.js
+++ sectionLinkCorrection.js
@@ -5,7 +5,7 @@ $('nav a, .bookexit').each(function(){
     $('.editor_atto_toolbar div').css('display', 'inline-block');
     $('.mceToolbarRow2').css('display', 'table');
     $('.mceToolbarRow3').css('display', 'table');
-    $('#arts-guide').remove();    
+    $('#arts-guide').remove(); 
     });
 $(".block-region .type_course a").each(function(){
     if($(this).text().match(/Turn editing/)){
