4f792d2255d3537808f9b04c328e63784a2605ac
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2020-09-29 10:31:38 +1000

    Update enableTooltip.js

--- enableTooltip.js
+++ enableTooltip.js
@@ -1,3 +1,9 @@
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js";
+s.integrity = "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN";
+s.crossorigin = "anonymous";
+$("head").append(s);
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
