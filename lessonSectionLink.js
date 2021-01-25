26033c6e79c357a95a2186d1ef40e30b2155549b
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-01-25 14:52:28 +1100

    Update lessonSectionLink.js

--- lessonSectionLink.js
+++ lessonSectionLink.js
@@ -3,15 +3,15 @@ window.onload=function(){
   $('nav a, .bookexit').each(function(){
         this.href=this.href.replace('#section-','&section=');
     });
-  $('.generalbox a:last-child').attr('href', $('ol.breadcrumb li:nth-last-child(2) > a').attr('href'));
-  $('.generalbox p:last-child').prepend('<h5>You have completed the activity <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>, Where to next?</h5>');
-  $('.generalbox p:last-child').append('<ul style="color:black;"><li><i class="fa fa-hand-o-left" aria-hidden="true"></i> You can review this lesson material again before continuing</li><li> Or you can continue onto the next activity <i class="fa fa-hand-o-right" aria-hidden="true"></i></li><li>Or return to the week this lesson is part of <i class="fa fa-reply" aria-hidden="true"></i></li></ul>');
-  $('.generalbox a.lessonbutton:nth-child(3)').prepend('<i class="fa fa-hand-o-left" aria-hidden="true"></i> ');
-  $('.generalbox a.lessonbutton:nth-child(4)').append(' <i class="fa fa-hand-o-right" aria-hidden="true"></i>');
+  $('.boxaligncenter a:last-child').attr('href', $('ol.breadcrumb li:nth-last-child(2) > a').attr('href'));
+  $('.boxaligncenter p:last-child').prepend('<h5>You have completed the activity <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>, Where to next?</h5>');
+  $('.boxaligncenter p:last-child').append('<ul style="color:black;"><li><i class="fa fa-hand-o-left" aria-hidden="true"></i> You can review this lesson material again before continuing</li><li> Or you can continue onto the next activity <i class="fa fa-hand-o-right" aria-hidden="true"></i></li><li>Or return to the week this lesson is part of <i class="fa fa-reply" aria-hidden="true"></i></li></ul>');
+  $('.boxaligncenter a.lessonbutton:nth-child(3)').prepend('<i class="fa fa-hand-o-left" aria-hidden="true"></i> ');
+  $('.boxaligncenter a.lessonbutton:nth-child(4)').append(' <i class="fa fa-hand-o-right" aria-hidden="true"></i>');
   var linkUrl=$('ol.breadcrumb li:nth-last-child(2) > a').html();
-  $('.generalbox a:last-child').html(linkUrl);
-  $('.generalbox a:last-child').append(' <i class="fa fa-reply"></i>');
-  $('.generalbox a:last-child').prepend('Return to ');
+  $('.boxaligncenter a:last-child').html(linkUrl);
+  $('.boxaligncenter a:last-child').append(' <i class="fa fa-reply"></i>');
+  $('.boxaligncenter a:last-child').prepend('Return to ');
   $('.progress-bar').append(' <span>Complete! <i class="fa fa-flag-checkered" aria-hidden="true"></i></span>');
 };
 };
