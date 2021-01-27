88d0120a4373b5bfb226aba2f5af9138760cea42
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-01-27 12:29:51 +1100

    Update lessonSectionLink.js

--- lessonSectionLink.js
+++ lessonSectionLink.js
@@ -1,8 +1,8 @@
 window.onload=function(){
-   if (window.location.href.indexOf("pageid=-9") > -1) {
-  $('nav a, .bookexit').each(function(){
+    $('nav a, .bookexit').each(function(){
         this.href=this.href.replace('#section-','&section=');
     });
+   if (window.location.href.indexOf("pageid=-9") > -1) {
   $('.boxaligncenter a:last-child').attr('href', $('ol.breadcrumb li:nth-last-child(2) > a').attr('href'));
     $('.boxaligncenter p').prepend('<h5>You have completed the activity <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>, Where to next?</h5>');
   $('.boxaligncenter p').append('<ul style="color:black;"><li><i class="fa fa-hand-o-left" aria-hidden="true"></i> You can review this lesson material again before continuing</li><li> Or you can continue onto the next activity <i class="fa fa-hand-o-right" aria-hidden="true"></i></li><li>Or return to the section this lesson is part of <i class="fa fa-reply" aria-hidden="true"></i></li></ul>');
