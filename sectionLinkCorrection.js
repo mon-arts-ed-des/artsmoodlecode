1ca1d2e7834664f10090ac1223d7eed54f7f9c0b
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-02-22 13:07:29 +1100

    Update sectionLinkCorrection.js

--- sectionLinkCorrection.js
+++ sectionLinkCorrection.js
@@ -50,6 +50,7 @@ window.onload=function(){
 			//link to how to's in the uni wide banner and nav drawer
 					$( ".header-right" ).prepend('<div class="custom-menus my-auto"><a href="https://lms.monash.edu/course/view.php?id=48399&section=11" target="_blank" class="border border-dark rounded-circle text-dark p-2" role="button" title="Ed Tech how tos"><i class="fa fa-bolt fa-fw" aria-hidden="true"></i></a></div>');
 	    				$( "#nav-drawer .list-group" ).prepend('<a class="list-group-item list-group-item-action" href="https://lms.monash.edu/course/view.php?id=48399&section=11" target="_blank" data-key="coursehome" data-isexpandable="0" data-indent="0" data-showdivider="0" data-type="60" data-nodetype="0" data-collapse="0" data-forceopen="0" data-isactive="0" data-hidden="0" data-preceedwithhr="0"><div class="ml-0"><div class="media"><span class="media-left"><i class="icon fa fa-bolt fa-fw " aria-hidden="true"></i></span><span class="media-body ">Ed Tech how tos</span></div></div></a>');
+					$('.section_action_menu .editing_delete').remove();
 			$("#nav-drawer .list-group a").each(function(){
 				if($(this).text().match(/Staff resources/)){
 
