7383bb4a491d8136548c255891d11e5ab270729b
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-03-19 17:01:22 +1100

    Update lessonSectionLink.js

--- lessonSectionLink.js
+++ lessonSectionLink.js
@@ -18,21 +18,23 @@ window.addEventListener('load', function(){
      $('nav a, .bookexit').each(function(){
         this.href=this.href.replace('#section-','&section=');
     });
+
 if (window.location.href.indexOf("pageid=-9") > -1) {
+        $('.boxaligncenter p').css('display','none');
+        $('.boxaligncenter .center').css('display','none');
         $('.boxaligncenter a:last-child').attr('href', $('ol.breadcrumb li:nth-last-child(2) > a').attr('href'));
-        $('.boxaligncenter p').prepend('<h5>You have completed the activity <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>, Where to next?</h5>');
-        $('.boxaligncenter p').append('<ul style="color:black;"><li><i class="fa fa-hand-o-left" aria-hidden="true"></i> You can review this lesson material again before continuing</li><li> Or you can continue onto the next activity <i class="fa fa-hand-o-right" aria-hidden="true"></i></li><li>Or return to the section this lesson is part of <i class="fa fa-reply" aria-hidden="true"></i></li></ul>');
-        $('.boxaligncenter a.lessonbutton:nth-child(3)').prepend('<i class="fa fa-hand-o-left" aria-hidden="true"></i> ');
-        $('.boxaligncenter a.lessonbutton:nth-child(4)').append(' <i class="fa fa-hand-o-right" aria-hidden="true"></i>');
+        $('.boxaligncenter p').before('<h5>You have completed the activity <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>, Where to next?</h5>');
+        $('.boxaligncenter .center').before('<h5>You have completed the activity <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>, Where to next?</h5>');
+        $('.boxaligncenter p').after('<ul style="color:black;"><li><i class="fa fa-hand-o-left" aria-hidden="true"></i> You can review this lesson material again before continuing (if made available in this lesson)</li><li> Or you can continue onto the next activity (if made available in this lesson) <i class="fa fa-hand-o-right" aria-hidden="true"></i></li><li>Or return to the section this lesson is part of <i class="fa fa-reply" aria-hidden="true"></i></li></ul>');
+        $('.boxaligncenter .center').after('<ul style="color:black;"><li><i class="fa fa-hand-o-left" aria-hidden="true"></i> You can review this lesson material again before continuing (if made available in this lesson)</li><li> Or you can continue onto the next activity (if made available in this lesson) <i class="fa fa-hand-o-right" aria-hidden="true"></i></li><li>Or return to the section this lesson is part of <i class="fa fa-reply" aria-hidden="true"></i></li></ul>');
+        $('.boxaligncenter a.lessonbutton:nth-last-child(2)').prepend('<i class="fa fa-hand-o-left" aria-hidden="true"></i>&nbsp;&nbsp;');
+        $('.boxaligncenter a.lessonbutton:nth-last-child(1)').append(' <i class="fa fa-hand-o-right" aria-hidden="true"></i>');
         var linkUrl=$('ol.breadcrumb li:nth-last-child(2) > a').html();
         $('.boxaligncenter a:last-child').html(linkUrl);
-        $('.boxaligncenter a:last-child').append(' <i class="fa fa-reply"></i>');
+        $('.boxaligncenter a:last-child').append('&nbsp;&nbsp;<i class="fa fa-reply"></i>');
         $('.boxaligncenter a:last-child').prepend('Return to ');
-        $('.progress-bar').append(' <span>Complete! <i class="fa fa-flag-checkered" aria-hidden="true"></i></span>');
     }
     else {
   //do nothing
     };
 });
-
-   
