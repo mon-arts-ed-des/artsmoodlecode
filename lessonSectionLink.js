8e02623d4abcc86ffd47a514f710607249840338
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-03-19 12:51:36 +1100

    Update lessonSectionLink.js

--- lessonSectionLink.js
+++ lessonSectionLink.js
@@ -1,9 +1,24 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+if (window.location.href.indexOf("pageid=-9") > -1) {
+link=document.createElement('link');
+link.rel='stylesheet';
+link.href='https://mon-arts-ed-des.github.io/artsmoodlecode/endOfLessonBlock.css';
+link.type="text/css"
+document.getElementsByTagName('head')[0].appendChild(link);
+}
+    else{
+link=document.createElement('link');
+link.rel='stylesheet';
+link.href='https://mon-arts-ed-des.github.io/artsmoodlecode/lessonBlock.css';
+link.type="text/css"
+document.getElementsByTagName('head')[0].appendChild(link);
+    }
+});
 window.addEventListener('load', function(){
-    $('nav a, .bookexit').each(function(){
+     $('nav a, .bookexit').each(function(){
         this.href=this.href.replace('#section-','&section=');
     });
-    if (window.location.href.indexOf("pageid=-9") > -1) {
-        $('head').append('<link rel="stylesheet" href="https://mon-arts-ed-des.github.io/artsmoodlecode/endOfLessonBlock.css" />')
+if (window.location.href.indexOf("pageid=-9") > -1) {
         $('.boxaligncenter a:last-child').attr('href', $('ol.breadcrumb li:nth-last-child(2) > a').attr('href'));
         $('.boxaligncenter p').prepend('<h5>You have completed the activity <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>, Where to next?</h5>');
         $('.boxaligncenter p').append('<ul style="color:black;"><li><i class="fa fa-hand-o-left" aria-hidden="true"></i> You can review this lesson material again before continuing</li><li> Or you can continue onto the next activity <i class="fa fa-hand-o-right" aria-hidden="true"></i></li><li>Or return to the section this lesson is part of <i class="fa fa-reply" aria-hidden="true"></i></li></ul>');
@@ -16,6 +31,8 @@ window.addEventListener('load', function(){
         $('.progress-bar').append(' <span>Complete! <i class="fa fa-flag-checkered" aria-hidden="true"></i></span>');
     }
     else {
-        $('head').append('<link rel="stylesheet" href="https://mon-arts-ed-des.github.io/artsmoodlecode/lessonBlock.css" />')
+  //do nothing
     };
 });
+
+   
