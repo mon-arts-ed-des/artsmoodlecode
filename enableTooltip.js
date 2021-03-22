e16301be0fc5286e973048ad5c0625cd1155487b
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-03-22 13:31:11 +1100

    Update enableTooltip.js

--- enableTooltip.js
+++ enableTooltip.js
@@ -1,32 +1,32 @@
+  //wait for window to load
 window.addEventListener('load', function(){
+  //run function to check if jQuery has loaded
 checkJQuery();
 function checkJQuery(){
+  //if jQuery hasn't loaded, wait 100ms and check again, once loaded run the function runTooltipJS to initialise tooltips
 if (typeof(jQuery)==='undefined'){setTimeout(checkJQuery,100);}else{runTooltipJS();}
 }
+  //function to initialise and style tooltips on the page
 function runTooltipJS(){
+  //use Liberate's loadjscssfile function to append a CSS file that styles tooltips to the head element
 loadjscssfile("https://mon-arts-ed-des.github.io/artsmoodlecode/tooltipStyle.css", "css");
+  //create a script element
 var t = document.createElement("script");
+  //create a text attribute
 t.type = "text/javascript";
+  //create a src attribute
 t.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
+  //create an integrity attribute
 t.integrity = "sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q";
+  //create a crossorigin attribute
 t.crossorigin = "anonymous";
+  //append the popperJS script to the body
 $("body").append(t);
+  //once page has loaded run the checkPopper function
 t.onload=checkPopper();
 }
+  //checkPopper function to initialise tooltips on the page according to Bootstrap specs
 function checkPopper(){
 try{$('[data-toggle="tooltip"]').tooltip();}catch(ex){setTimeout(checkPopper,100);
 }
 });
-/*var s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js";
-s.integrity = "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN";
-s.crossorigin = "anonymous";
-var t = document.createElement("script");
-t.type = "text/javascript";
-t.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
-t.integrity = "sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q";
-t.crossorigin = "anonymous";
-$("head").append(s);
-$("head").append(t);
-$('head').append('<script>$(document).ready(function(){ $(\'[data-toggle="tooltip"]\').tooltip();});</script>');*/
