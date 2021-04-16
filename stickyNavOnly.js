9b872145cb668ab93218ed6281ce00425f4c46ec
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-04-16 15:47:06 +1000

    Update stickyNavOnly.js

--- stickyNavOnly.js
+++ stickyNavOnly.js
@@ -1,19 +1,20 @@
- window.addEventListener("load",function set_sticky_nav(){
-   var stick_height, navbar;
-   var breadcrumbs=document.getElementById("page-navbar");
-   navbar=document.getElementById("section-0");
-   stick_height=breadcrumbs.offsetTop;
-   stick_height-=15;
+window.addEventListener("load",function set_sticky_nav(){
+	var stick_height, navbar;
+	var breadcrumbs=document.getElementById("page-navbar");
+	navbar=document.getElementById("section-0");
+	stick_height=breadcrumbs.offsetTop;
+	stick_height-=15;
 	var nav_drawer=document.getElementById("nav-drawer");
-
 	var nav_drawer_top=nav_drawer.offsetTop;
-  if(window.pageYOffset>=stick_height){
-    nav_drawer.setAttribute("data-region2","drawer");
-    navbar.classList.add("sticky");
-
-  }else{
-    nav_drawer.removeAttribute("data-region2");
-    nav_drawer.style.top=nav_drawer_top;
-    navbar.classList.remove("sticky");
-  }
+		if(window.pageYOffset>=stick_height){
+   			nav_drawer.setAttribute("data-region2","drawer");
+    			//navbar.classList.add("sticky");
+			$(navbar).addClass("sticky");
+  		}
+		else{
+   			nav_drawer.removeAttribute("data-region2");
+    			nav_drawer.style.top=nav_drawer_top;
+    			//navbar.classList.remove("sticky");
+			$(navbar).removeClass("sticky");
+  		}
 });
