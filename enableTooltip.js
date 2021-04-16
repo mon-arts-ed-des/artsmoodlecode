94bf12dfd8faa84cd8c22c4e78a806268df0e34a
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-04-16 15:23:13 +1000

    Update enableTooltip.js

--- enableTooltip.js
+++ enableTooltip.js
@@ -1,32 +1,37 @@
   //wait for window to load
 window.addEventListener('load', function(){
   //run function to check if jQuery has loaded
-checkJQuery();
-function checkJQuery(){
+      checkJQuery();
+      function checkJQuery(){
   //if jQuery hasn't loaded, wait 100ms and check again, once loaded run the function runTooltipJS to initialise tooltips
-if (typeof(jQuery)==='undefined'){setTimeout(checkJQuery,100);}else{runTooltipJS();}
-}
+        if (typeof(jQuery)==='undefined'){
+          setTimeout(checkJQuery,100);
+        }
+        else{
+          runTooltipJS();
+        }
+      }
   //function to initialise and style tooltips on the page
-function runTooltipJS(){
+      function runTooltipJS(){
   //use Liberate's loadjscssfile function to append a CSS file that styles tooltips to the head element
-loadjscssfile("https://mon-arts-ed-des.github.io/artsmoodlecode/tooltipStyle.css", "css");
+        loadjscssfile("https://mon-arts-ed-des.github.io/artsmoodlecode/tooltipStyle.css", "css");
   //create a script element
-var t = document.createElement("script");
+        var t = document.createElement("script");
   //create a text attribute
-t.type = "text/javascript";
+        t.type = "text/javascript";
   //create a src attribute
-t.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
+        t.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
   //create an integrity attribute
-t.integrity = "sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q";
+        t.integrity = "sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q";
   //create a crossorigin attribute
-t.crossorigin = "anonymous";
+        t.crossorigin = "anonymous";
   //append the popperJS script to the body
-$("body").append(t);
+        $("body").append(t);
   //once page has loaded run the checkPopper function
-t.onload=checkPopper();
-}
+        t.onload=checkPopper();
+      }
   //checkPopper function to initialise tooltips on the page according to Bootstrap specs
-function checkPopper(){
-try{$('[data-toggle="tooltip"]').tooltip();}catch(ex){setTimeout(checkPopper,100);
-}
+     function checkPopper(){
+        try{$('[data-toggle="tooltip"]').tooltip();}catch(ex){setTimeout(checkPopper,100)};
+       }
 });
