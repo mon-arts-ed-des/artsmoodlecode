85162a20aee48b838ed119ac6301b671ed7f41f6
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-05-05 11:20:44 +1000

    Update autoNav.js

--- autoNav.js
+++ autoNav.js
@@ -141,13 +141,13 @@ function get_topics(){
 	var full='<div id="arts-dropdown" class="arts-banner-dropdown"><a href="javascript:void(0);"><i class="fa fa-chevron-circle-down fa-fw" aria-hidden="true"></i> <span class="arts-banner-dropdown-link">'+dropdownBtn+' <i class="fa fa-caret-down"></i></span></a><div class="arts-banner-dropdown-content">'+tmp+'</div></div>';
 
 	nav_array.push(full);
-	var dropdownText = $('.arts-banner-dropdown-content a').text()
+	/*var dropdownText = $('.arts-banner-dropdown-content a').text()
 	if(dropdownText.indexOf('Topic') != -1){
 		var dropdownBtn = 'Topics'
 	}
 	else{
 		var dropdownBtn = 'Weeks'
-	}
+	}*/
 	
 }
 
