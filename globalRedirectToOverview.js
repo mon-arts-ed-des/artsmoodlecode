fd6f3ec8eb3f04bda0bf8741569fff0cb42ec504
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-05-13 13:16:39 +1000

    Update globalRedirectToOverview.js

--- globalRedirectToOverview.js
+++ globalRedirectToOverview.js
@@ -16,8 +16,9 @@ window.addEventListener('load', function set_start(){
 	var filesRD = window.location.href.indexOf('files')
 	var adminRD = window.location.href.indexOf('admin')
 	var previewRD = window.location.href.indexOf('preview')
+	var bookRD = window.location.href.indexOf('book')
 	//section is null so it must be the entry page
-	if (sectionNumberRD == -1 && editScreenRD == -1 && completionRD == -1 && userRD == -1 && completionRD == -1 && enrolRD == -1 && groupRD == -1 && rolesRD == -1 && filterRD == -1 && reportRD == -1 && gradeRD == -1 && backupRD == -1 && resetRD == -1 && questionRD == -1 && filesRD == -1 && adminRD == -1 && previewRD == -1 || hashSectionRD > -1) {
+	if (sectionNumberRD == -1 && editScreenRD == -1 && completionRD == -1 && userRD == -1 && completionRD == -1 && enrolRD == -1 && groupRD == -1 && rolesRD == -1 && filterRD == -1 && reportRD == -1 && gradeRD == -1 && backupRD == -1 && resetRD == -1 && questionRD == -1 && filesRD == -1 && adminRD == -1 && previewRD == -1 && bookRD == -1 || hashSectionRD > -1) {
 		//find the link that has the text Overview or Welcome in it and take its href value and assign it to a variable
 		var overviewSection=$("nav a:contains('Overview'), nav a:contains(â€˜Welcomeâ€™)").attr('href');
 		//change the current window address to the new section
