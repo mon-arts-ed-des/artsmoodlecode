c1e57a78fcedf5524d7a2bc176f11c73416945e1
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-05-13 11:26:27 +1000

    Update autoNav.js

--- autoNav.js
+++ autoNav.js
@@ -186,7 +186,19 @@ function set_click(){
 	
 	$(".arts-banner-dropdown").on("click",function(){
       var obj=$(this).find(".arts-banner-dropdown-content");
-			obj.width($("#arts-dropdown").width());
+		var lengths = $(".arts-banner-dropdown-content a").map(function(i, el) {
+    		return $(el).text().length;
+		}).get();
+		var maxLength = Math.max.apply(this, lengths);
+		console.log(maxLength);
+		if (maxLength > 8){
+			$('.arts-banner-dropdown-content').width('unset');
+		}
+		else {
+			$(".arts-banner-dropdown-content").width($("#arts-dropdown").width());
+		}		
+		if($(window).width()<1000)$(".arts-banner-dropdown-content").css('width','100%');
+	
 			var id1=$(this).attr("id");
 			
 			$(".arts-banner-dropdown-content").each(function(){
@@ -195,7 +207,7 @@ function set_click(){
 					if($(this).css("display") == "block"){$(this).stop(true).slideToggle(); }
 				}
 			});
-			
+
       obj.stop(true).slideToggle();
     });
 	
@@ -215,9 +227,18 @@ var dropdownText = $('.arts-banner-dropdown-content a').text()
 		if(dropdownText.indexOf("Topic") != -1){
 			$('.arts-banner-dropdown-link').html('Topics <i class="fa fa-caret-down"></i>')
 		}
-		else{
+		else if(dropdownText.indexOf("Week") != -1){
 			$('.arts-banner-dropdown-link').html('Weeks <i class="fa fa-caret-down"></i>')
-            }
+       		}
+		else if(dropdownText.indexOf("Block") != -1){
+			$('.arts-banner-dropdown-link').html('Blocks <i class="fa fa-caret-down"></i>')
+            	}
+		else if(dropdownText.indexOf("Module") != -1){
+			$('.arts-banner-dropdown-link').html('Modules <i class="fa fa-caret-down"></i>')
+            	}
+		else if(dropdownText.indexOf("Day") != -1){
+			$('.arts-banner-dropdown-link').html('Days <i class="fa fa-caret-down"></i>')
+	        }
 }
 
 function set_start(){
