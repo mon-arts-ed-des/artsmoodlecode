21e9df8b2034f85ee4715dfcd9e0331e6608f551
Nigel <nigel.thorne@bigpond.com>
2021-05-15 13:05:52 +1000

    Update

--- arts_cq.js
+++ arts_cq.js
@@ -59,7 +59,10 @@ function startJS(){
 }
 
 
-
+function highlight(obj,num){
+	$(".slider-text").eq(num).find("span").removeClass("slider-highlight");
+	if(obj>0)$(".slider-text").eq(num).find("span:eq("+obj+")").addClass("slider-highlight");
+}
 
 
 function changeLinks(){
