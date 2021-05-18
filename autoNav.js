989059636a1c171bad50e7881e7955ca3fa69879
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-05-18 11:29:36 +1000

    Update autoNav.js

--- autoNav.js
+++ autoNav.js
@@ -259,10 +259,11 @@ function set_start(){
 	var filesRD = window.location.href.indexOf('files')
 	var adminRD = window.location.href.indexOf('admin')
 	var previewRD = window.location.href.indexOf('preview')
+	var bookRD = window.location.href.indexOf('book')
 	//section is null so it must be the entry page
-	if (sectionNumberRD == -1 && editScreenRD == -1 && completionRD == -1 && userRD == -1 && completionRD == -1 && enrolRD == -1 && groupRD == -1 && rolesRD == -1 && filterRD == -1 && reportRD == -1 && gradeRD == -1 && backupRD == -1 && resetRD == -1 && questionRD == -1 && filesRD == -1 && adminRD == -1 && previewRD == -1 || hashSectionRD > -1) {
+	if (sectionNumberRD == -1 && editScreenRD == -1 && completionRD == -1 && userRD == -1 && completionRD == -1 && enrolRD == -1 && groupRD == -1 && rolesRD == -1 && filterRD == -1 && reportRD == -1 && gradeRD == -1 && backupRD == -1 && resetRD == -1 && questionRD == -1 && filesRD == -1 && adminRD == -1 && previewRD == -1 && bookRD == -1 || hashSectionRD > -1) {
 		//find the link that has the text Overview or Welcome in it and take its href value and assign it to a variable
-		var overviewSection=$("nav a:contains('Overview'), nav a:contains(â€˜Welcomeâ€™)").attr('href');
+		var overviewSection=$("nav a:contains('Overview'), nav a:contains('Welcome'), nav a:contains('Home')").attr('href');
 		//change the current window address to the new section
 		overviewSection=overviewSection.replace(/#section-/g, "&section=");
 		window.location.href = overviewSection;
