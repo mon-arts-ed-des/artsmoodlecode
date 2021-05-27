57887cf2778e5e9dd86c1cda97402b44ac460641
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-05-27 10:56:01 +1000

    Update autoNav.js

--- autoNav.js
+++ autoNav.js
@@ -5,7 +5,7 @@ var home_array=["welcome","overview","home"];
 var resources_array=["study resources","resources"];
 var assessment_array=["assessment","exam","test"];
 var forum_array=["forum","communication","communication/faqs"];
-var topics_array=["topic","week","module","day","block"];
+var topics_array=["week","topic","module","block","day"];
 var stick_height, navbar;
 var start_section;
 var dropdownText;
