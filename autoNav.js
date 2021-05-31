377c426272a829a322e55746a6c42c70e4e0ba9f
mon-arts-ed-des <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-05-31 11:21:44 +1000

    Update autoNav.js

--- autoNav.js
+++ autoNav.js
@@ -1,3 +1,4 @@
+//Create arrays to obtains all the required names and links and sort them into their various buckets.
 var log_this=true;
 var orig_array=[];
 var nav_array=[];
@@ -5,13 +6,23 @@ var home_array=["welcome","overview","home"];
 var resources_array=["study resources","resources"];
 var assessment_array=["assessment","exam","test"];
 var forum_array=["forum","communication","communication/faqs"];
-var topics_array=["week","topic","module","block","day"];
+var topics_array=["week","topic","module","day","block"];
+var zoom_array=["zoom","zoom sessions","zoom workshops","zoom tutorials","zoom links"]
 var stick_height, navbar;
 var start_section;
 var dropdownText;
 
-checkJQuery();
-function checkJQuery(){if (typeof(jQuery)==='undefined'){setTimeout(checkJQuery,100);}else{get_nav();}}
+
+set_start();
+
+//Check jQuery is working on the site.
+function checkJQuery(){
+	if (typeof(jQuery)==='undefined'){
+		setTimeout(checkJQuery,100);}
+	else{
+		get_nav();
+	}
+}
 
 //push all the nav items into an array - all we care about is name and URL
 function get_nav(){
@@ -21,12 +32,12 @@ function get_nav(){
 		var new_obj=new Object();
 		new_obj.name=$(this).prop("innerText").trim();
 		new_obj.href=$(this).attr("href");
+		new_obj.section=new_obj.href.substring(new_obj.href.indexOf("section=")+8,new_obj.href.length);
 		orig_array.push(new_obj);
-		
 	});
 	if(log_this)console.log(orig_array);
-	
-	
+
+
 	//get navbar section so we can apply styles to it
 	navbar=document.getElementById("section-0");
 
@@ -36,26 +47,29 @@ function get_nav(){
 	//remove 15 for the padding in the main panel
 	stick_height-=15;
 	window.onscroll=function(){set_sticky_nav()};
+	//resize the dropdown content to fit the nav
 	$(window).resize(function(){
-		$(".arts-banner-dropdown-content").width($("#arts-dropdown").width());		
+		$(".arts-banner-dropdown-content").width($("#arts-dropdown").width());
 		if($(window).width()<1000)$(".arts-banner-dropdown-content").css('width','100%');
 	});
-
-	set_start();
-	
+	//run all functions to sort arrays and build nav
 	get_home();
 	get_topics();
 	get_assessment();
 	get_forum();
 	get_resources();
+	get_zoom();
 	get_grades();
-	
 	build_nav();
 	set_click();
 	dropdownParent();
-	
+
 }
 
+
+function show_nav(){
+
+}
 
 
 function get_home(){
@@ -74,7 +88,21 @@ function get_home(){
 	
 }
 
-
+function get_zoom(){
+	if(log_this)console.log("------------------------ get zoom ------------------------");
+	
+	$.each(zoom_array,function(i,word){
+		$.each(orig_array,function(x,obj){
+			if(word==obj.name.toLowerCase()){
+				if(log_this)console.log("FOUND "+word+" AT "+x);
+				var tmp='<div id="arts-zoom"><a href="'+obj.href+'"><i class="fa fa-video-camera fa-fw" aria-hidden="true"></i> '+obj.name+'</a></div>';
+				nav_array.push(tmp);
+				if(!start_section)start_section=obj.href;
+			}
+		});	
+	});
+	
+}
 
 function get_resources(){
 	if(log_this)console.log("------------------------ get resources ------------------------");
@@ -121,7 +149,6 @@ function get_forum(){
 			}
 		});	
 	});
-	
 }
 
 function get_topics(){
@@ -134,19 +161,19 @@ function get_topics(){
 			
 			if(obj.name.toLowerCase().indexOf(word)!=-1){
 				if(log_this)console.log("FOUND "+word+" AT "+x);
-				tmpArray.push({href:obj.href,name:obj.name});
+				tmpArray.push({href:obj.href,name:obj.name,section:obj.section});
 			}
 		});	
 	});
 
-	console.log("-------------------");
-	console.log(tmpArray);
+	if(log_this)console.log(tmpArray);
 	tmpArray=makeArrayUnique(tmpArray);
-	console.log(tmpArray);
+	if(log_this)console.log(tmpArray);
+	tmpArray=sortBySection(tmpArray);
+	if(log_this)console.log(tmpArray);
 
-	$.each(tmpArray,function(i,val) {
-		tmp+='<a href="'+val.href+'">'+val.name+'</a>';
-	});
+
+	$.each(tmpArray,function(i,val){tmp+='<a href="'+val.href+'">'+val.name+'</a>';});
 
 	var full='<div id="arts-dropdown" class="arts-banner-dropdown"><a href="javascript:void(0);"><i class="fa fa-chevron-circle-down fa-fw" aria-hidden="true"></i> <span class="arts-banner-dropdown-link"> <i class="fa fa-caret-down"></i></span></a><div class="arts-banner-dropdown-content">'+tmp+'</div></div>';
 
@@ -164,6 +191,12 @@ function makeArrayUnique(tmpArr1){
 	});
 	return tmpArr2;
 }
+
+function sortBySection(tmpArr1){
+	tmpArr1.sort(function(a,b){return a.section-b.section;});
+	return tmpArr1;
+}
+
 
 function get_grades(){
 	if(log_this)console.log("------------------------ get grades ------------------------");
@@ -190,7 +223,7 @@ function build_nav(){
 	nav_menu=nav_menu.replace(/#section-/g, "&section=");
 
 	$(".summary .no-overflow").first().append(nav_menu);	
-		
+	
 }
 
 function set_click(){
@@ -254,7 +287,11 @@ var dropdownText = $('.arts-banner-dropdown-content a').text()
 }
 
 function set_start(){
-	var sectionNumberRD = window.location.href.indexOf('section')
+
+	//var queryArr=['section','edit','completion','#section-','user','enrol','group','roles','filter','report','grade','backup','reset','question','files','admin','preview'];
+	//$.inArray(window.location.href,queryArr);
+
+	/*var sectionNumberRD = window.location.href.indexOf('section')
 	var editScreenRD = window.location.href.indexOf('edit')
 	var completionRD = window.location.href.indexOf('completion')
 	var hashSectionRD = window.location.href.indexOf('#section-')
@@ -270,20 +307,50 @@ function set_start(){
 	var questionRD = window.location.href.indexOf('question')
 	var filesRD = window.location.href.indexOf('files')
 	var adminRD = window.location.href.indexOf('admin')
-	var previewRD = window.location.href.indexOf('preview')
-	var bookRD = window.location.href.indexOf('book')
+	var previewRD = window.location.href.indexOf('preview')*/
+
+
 	//section is null so it must be the entry page
-	if (sectionNumberRD == -1 && editScreenRD == -1 && completionRD == -1 && userRD == -1 && completionRD == -1 && enrolRD == -1 && groupRD == -1 && rolesRD == -1 && filterRD == -1 && reportRD == -1 && gradeRD == -1 && backupRD == -1 && resetRD == -1 && questionRD == -1 && filesRD == -1 && adminRD == -1 && previewRD == -1 && bookRD == -1 || hashSectionRD > -1) {
+	//if (sectionNumberRD == -1 && editScreenRD == -1 && completionRD == -1 && userRD == -1 && completionRD == -1 && enrolRD == -1 && groupRD == -1 && rolesRD == -1 && filterRD == -1 && reportRD == -1 && gradeRD == -1 && backupRD == -1 && resetRD == -1 && questionRD == -1 && filesRD == -1 && adminRD == -1 && previewRD == -1 || hashSectionRD > -1) {
+
+	if(location.search.indexOf("&")==-1){
 		document.querySelector(".topics").remove();
 		document.querySelector("#toggle-all").remove();
 		document.querySelector("#topcoll-display-instructions").remove();
 		//find the link that has the text Overview or Welcome in it and take its href value and assign it to a variable
-		var overviewSection=$("nav a:contains('Overview'), nav a:contains('Welcome'), nav a:contains('Home')").attr('href');
+
+		//var overviewSection=$("nav a:contains('Overview'), nav a:contains('Welcome'), nav a:contains('Home')").attr('href');
+
 		//change the current window address to the new section
+		var navdr=document.getElementById('nav-drawer');
+		var atags=navdr.querySelectorAll("a");
+		var regex=/\b(?:overview|welcome|home)\b/gi;
+		for(var i=0;i<atags.length;i++){
+			if(atags[i].innerText.match(regex)){
+				overviewSection=atags[i].href;
+				break;
+			}
+		}
+
 		overviewSection=overviewSection.replace("#section-", "&section=");
 		window.location.href = overviewSection;
+	}else{
+		//on the right page now so start nav building
+		checkJQuery();
 	}
+
+
+
 }
+
+function contains(selector,text){
+	var elements=document.querySelectorAll(selector);
+	return Array.prototype.filter.call(elements,function(element){
+		return RegExp(text).test(element.textContent);
+	});
+}
+
+
 function set_sticky_nav(){
 	var nav_drawer=document.getElementById("nav-drawer");
 
