aaa9a88d113e78d7bc0480fd49fd14fc2cea6756
Monash Faculty of Arts <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-07-26 10:14:40 +1000

    Update removeAssessmentPenaltyText.js

--- removeAssessmentPenaltyText.js
+++ removeAssessmentPenaltyText.js
@@ -1,9 +1,16 @@
+//Wait until the window loads, then run the function removePenaltyText, and if it has run, stop running it
 window.addEventListener("load", removePenaltyText, false);
+//function to remove the text
 function removePenaltyText(){
+  //Set timeout means to run a function on a delay. This one waits until 1000ms after the loading of the page then runs the function to remove the penalty text.
   setTimeout(function(){
+    //find the assessmentPenaltyText ID
     var assessmentPenaltyText=document.getElementById("assessmentPenaltyText");
+    //remove the text
     assessmentPenaltyText.remove();
+    //log that the text is removed to the console
 console.log('Penalty text removed')
+    //delay this function by 1000ms
   }, 1000
             );
 };
