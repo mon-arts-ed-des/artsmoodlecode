219c9ad8c3b1454a896657cf1d473d45670260c0
Monash Faculty of Arts <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-08-06 13:12:42 +1000

    Update sectionLinkCorrection.js

--- sectionLinkCorrection.js
+++ sectionLinkCorrection.js
@@ -99,7 +99,7 @@ window.onload=function(){
 					$('.section_action_menu .editing_delete').css('display','none');
 					$('#inst3407551 .action-menu-trigger').css('display','none');
 					$('#inst3407542 .action-menu-trigger').css('display','none');
-					document.querySelector('#section-0 .summary .fa-cog').closest('a').style.display = "none"
+					document.querySelector('#section-0 .summary .fa-cog').closest('a').style.display = "none";
 					$("#nav-drawer .list-group a").each(function(){
 						if($(this).text().match(/Staff resources/)){
 
@@ -142,7 +142,7 @@ window.onload=function(){
 			$('.section_action_menu .editing_delete').css('display','block');
 			$('#inst3407551 .action-menu-trigger').css('display','block');
 			$('#inst3407542 .action-menu-trigger').css('display','block');
-			document.querySelector('#section-0 .summary .fa-cog').closest('a').style.display = "inline-block"
+			document.querySelector('#section-0 .summary .fa-cog').closest('a').style.display = "inline-block";
 //Query the logged in user block and find the user's name
 			var userName = document.querySelector('.myprofileitem.fullname')
 			? document.querySelector('.myprofileitem.fullname').innerText
