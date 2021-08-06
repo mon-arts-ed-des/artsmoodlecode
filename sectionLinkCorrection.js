30647807ed5ecc52b3c00882b2adb47c2b6363af
Monash Faculty of Arts <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-08-06 13:35:40 +1000

    Update sectionLinkCorrection.js

--- sectionLinkCorrection.js
+++ sectionLinkCorrection.js
@@ -149,7 +149,13 @@ window.onload=function(){
 			$('.section_action_menu .editing_delete').css('display','block');
 			$('#inst3407551 .action-menu-trigger').css('display','block');
 			$('#inst3407542 .action-menu-trigger').css('display','block');
-			//document.querySelector('#section-0 .summary .fa-cog').closest('a').style.display = "inline-block";
+			if (turnEditingOnBtnText == "Turn editing off"){
+				document.querySelector('#section-0 .summary .fa-cog').closest('a').style.display = "inline-block";
+				console.log('editing is on');
+			}
+			else {
+				console.log('editing is off');
+			}
 //Query the logged in user block and find the user's name
 			var userName = document.querySelector('.myprofileitem.fullname')
 			? document.querySelector('.myprofileitem.fullname').innerText
