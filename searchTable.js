4c973b2955067dfdbf3dc045ef8f5657d3b2c507
Monash Faculty of Arts <52515575+mon-arts-ed-des@users.noreply.github.com>
2021-09-03 13:00:55 +1000

    Update searchTable.js

--- searchTable.js
+++ searchTable.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".search-input").forEach((inputField) => {
     const tableRows = inputField
-      .nextElementSibling("table")
+      .nextSibling("table")
       .querySelectorAll("tbody > tr");
     const headerCell = inputField.closest("th");
     const otherHeaderCells = headerCell.closest("tr").children;
