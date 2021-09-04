6ea2b2235dac6ad0089cd9e65935fa496b3f5c2e
mon-arts-ed-des <arts-educational-design@monash.edu>
2021-09-04 21:31:20 +1000

    Attempt 11 - refining borders

--- searchTable.js
+++ searchTable.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchableCells = Array.from(tableRows).map(
       (row) => row.querySelectorAll("td")[columnIndex]
     );
-
     inputField.addEventListener("input", () => {
       const searchQuery = inputField.value.toLowerCase();
 
@@ -19,18 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
      //   row.style.visibility = "hidden";
         if (value.search(searchQuery) === -1 || value.search(searchQuery) === "" || value.search(searchQuery) === undefined || $('input').val().length == 0) {
           row.style.visibility = "collapse";
-          $('table').removeClass('table-bordered')
+          $('table').removeClass('table-bordered');
         }
         else{
           row.style.visibility = "visible";
-          $('table').addClass('table-bordered')
+          $('table').addClass('table-bordered');
         }
-        
       }
     });
   });
+  $('tbody tr').css('visibility','hidden');
+  $('td').css('border','none');
 });
-window.addEventListener("load",function(){
-$('tbody tr').css('visibility','hidden');
-$('td').css('border','none');
-})
+/*window.addEventListener("load",function(){
+
+})*/
